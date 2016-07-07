2c3d0f023161330dfb81899e09792bcdc401f969
LeeJinOh <jjanggu1990@naver.com>
2016-07-07 17:21:42 +0900

    Asfdsdaf

--- src/main/java/spring/controller/message/MessageController.java
+++ src/main/java/spring/controller/message/MessageController.java
@@ -6,15 +6,11 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
-
-import jdk.nashorn.internal.ir.RuntimeNode.Request;
 import mybatis.MemberDAO;
 import mybatis.MessageDAO;
 import spring.model.MemberCommand;
